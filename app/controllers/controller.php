ea6d10683f515fe687373e02efc2887d77849c9c
Haneen Abdulglil <Haneenabdulglil2018@yahoo.com>
2022-03-27 14:05:04 -0700

    create MVC project

---
+++ app/controllers/controller.php
@@ -0,0 +1,25 @@
+
+
+<?php
+
+class Controller {
+    public function model($model_name){
+        require_once 'app/models/'.$model_name.'.php';
+
+        return new $model_name();
+    }
+
+    public function view($view_name,$data=null){
+
+        require_once 'app/views/'.$view_name.'.php';
+
+    }
+
+
+    public function view_page($view_name,$data=null){
+
+        require_once 'app/views/users/'.$view_name.'.php';
+
+    }
+
+}
